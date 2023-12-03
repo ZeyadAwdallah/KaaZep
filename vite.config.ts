44ffa3cb651fe4cee24a768cd17282446b013606
zeyadawdallah <zeyadawdallah@gmail.com>
2023-12-03 21:12:31 +0200

    Update HomePage

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,17 +14,17 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/assets/icons/manifest-icon-192.maskable.png',
-            type: 'image',
+            src: '/icons/manifest-icon-192.maskable.png',
+            type: 'image/svg+xml', 
             sizes: '192x192',
           },
           {
-            src: '/assets/icons/manifest-icon-512.maskable.png',
-            type: 'image',
+            src: '/icons/manifest-icon-512.maskable.png',
+            type: 'image/svg+xml', 
             sizes: '512x512',
           },
         ],
       },
     }),
   ],
-});
+})
